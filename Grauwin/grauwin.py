101f504ec06fa918d38d69d97c9834059531c7be
sblascolopez <62884672+sblascolopez@users.noreply.github.com>
2020-03-30 14:51:02 +0200

    Update grauwin.py

--- Grauwin/grauwin.py
+++ Grauwin/grauwin.py
@@ -2,30 +2,8 @@
 
 import numpy as np
 from random import randint
+from classes import Ville, Paramètres
 
-class Paramètres :
-    """ Classe regroupant tous les paramètres d'une simulation """
-    
-    def __init__ (self,q,h,m,alpha = 0) :
-        self.q = q  # définit le nombre de quartier Q = q² > ici la variable q représente le q minuscule de la relation écrite, c'est ça ?
-        self.h = h  # définit le nombre d'emplacements par quartiers H = h² > même question pour h
-        self.l = h*q  # taille de la ville 
-        
-        self.N = ((h*q)**2)/2  # Nombre d'agents initialement 
-        self.alpha = alpha  # Coefficient intégrant l'altruisme
-        
-        self.u = lambda x : 2*x if x <= 1/2 else (2*(m-1)*x+2-m) # Fonction d'utilité des agents
-
-
-class Ville :
-    """Classe regroupant toutes les caractéristiques de la ville d'une simulation :
-        - la ville, sous la forme d'une grille de taille l * l, contenant des 0 si l'emplacement est occupé, des 1 s'il est occupé
-        - les densités de chaque quartier, sous la forme d'une grille q*q
-    """
-    
-    def __init__ (self,ville,densites) : 
-        self.ville = ville
-        self.densites = densites
 
 # Définissions une première fonction qui initialise la simulation en créant une ville 
         
