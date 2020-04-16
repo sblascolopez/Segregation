7a13c9342634c8999ed71d71745d3b2c348df259
sblascolopez <62884672+sblascolopez@users.noreply.github.com>
2020-04-16 17:52:39 +0200

    Add files via upload

--- Grauwin/complexite.py
+++ Grauwin/complexite.py
@@ -58,38 +58,7 @@ def cout_n_iter () :
 # Essayons désormais d'étudier la dépendance en q et h du temps d'exécution. On fixe donc un nombre d'exécutions n_iter quelconque. Comme on s'attend à des temps d'exécution en O(h²) et O(q²), on essaye de tracer ln(t) en fonction de ln(q) et ln(h)
 #Ici, j'ai substitué grauwin par init_ville car on a établit que la dépendance du temps d'éxécution en q et h n'intervient que dans cette fonction. Appeler grauwin a le double inconvénient d'augmenter inutilement le temps d'exécution des algorithmes suivants, et de rendre le calcul moins précis en ajoutant de nombreux pics dus au choix d'individus et d'emplacements vides aléatoires réalisés dans grauwin.
 
-def cout_h () :
-    q = 5
-    x=[]  #contient les différentes valeurs de h testées
-    y=[]  #contient le temps d'execution associé
-    for h in range(10,101) :   #on enlève les petites valeurs pour ne pas avoir un temps trop petit (et un problème lors du passage au log)
-        p = Paramètres(q,h,T,m,alpha)
-        x.append(log(h))
-        t=time()
-        init_ville(p)
-        t=time()-t
-        y.append(log(t))
-    #Réalisons ensuite une régression linéaire pour trouver a et b tels que log(t) = a*log(h) + b :
-    regressor = LinearRegression()
-    X = [[i] for i in x]
-    regressor.fit(X, y)
-    a = regressor.coef_[0]
-    b = regressor.intercept_
-    r2 = regressor.score(X,y)
-    y_droite = [a*i + b for i in x]
-    #On trace ensuite les points obtenus en effectuant les simulations, et la droite obtenue par régression linéaire. 
-    plt.plot(x,y, label = "log(t) en fonction de log(h)")
-    plt.plot(x,y_droite, label = "log(t)="+str(a)+"log(h)+"+str(b)+", r² ="+str(r2))
-    plt.title("Temps d'exécution t en fonction du nombre d'emplacements par quartiers h² (90 points)")
-    plt.xlabel("log(h)")
-    plt.ylabel("log(t) (t en secondes)")
-    plt.legend()  
-    plt.show()
-
-#cout_h()
-
-
-def cout_q () :   #Même fonction que cout_h en remplaçant h par q
+def cout_q () :   
     h = 20
     x=[]  #contient les différentes valeurs de h testées
     y=[]  #contient le temps d'execution associé
@@ -118,5 +87,36 @@ def cout_q () :   #Même fonction que cout_h en remplaçant h par q
     plt.show()
 
 #cout_q()
+    
+
+def cout_h () :  #Même fonction que cout_q en remplaçant q par h
+    q = 5
+    x=[]  #contient les différentes valeurs de h testées
+    y=[]  #contient le temps d'execution associé
+    for h in range(10,101) :   #on enlève les petites valeurs pour ne pas avoir un temps trop petit (et un problème lors du passage au log)
+        p = Paramètres(q,h,T,m,alpha)
+        x.append(log(h))
+        t=time()
+        init_ville(p)
+        t=time()-t
+        y.append(log(t))
+    #Réalisons ensuite une régression linéaire pour trouver a et b tels que log(t) = a*log(h) + b :
+    regressor = LinearRegression()
+    X = [[i] for i in x]
+    regressor.fit(X, y)
+    a = regressor.coef_[0]
+    b = regressor.intercept_
+    r2 = regressor.score(X,y)
+    y_droite = [a*i + b for i in x]
+    #On trace ensuite les points obtenus en effectuant les simulations, et la droite obtenue par régression linéaire. 
+    plt.plot(x,y, label = "log(t) en fonction de log(h)")
+    plt.plot(x,y_droite, label = "log(t)="+str(a)+"log(h)+"+str(b)+", r² ="+str(r2))
+    plt.title("Temps d'exécution t en fonction du nombre d'emplacements par quartiers h² (90 points)")
+    plt.xlabel("log(h)")
+    plt.ylabel("log(t) (t en secondes)")
+    plt.legend()  
+    plt.show()
+
+#cout_h()
 
 
